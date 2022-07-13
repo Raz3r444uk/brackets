615f8dbc0abf28c7024c4effa55dd611fa11c304
Гуськов Владимир <raz3r444uk@mail.ru>
2022-07-13 19:30:55 +0400

    init: init commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  // Сооздаем массив скобок
+  const bracketsArr = bracketsConfig.map((e) => {
+    return `${e[0]}${e[1]}`;
+  });
+
+  // Записываем изначально значение длины строки для последующего сравнения
+  let strLength = str.length;
+
+  while (str.length) {
+    // Удаляем все скобки из str, перезапиcывая её значение
+    bracketsArr.forEach((e) => {
+      str = str.replaceAll(e, "");
+    });
+
+    if (str.length === strLength) {
+      return false;
+    } else {
+      strLength = str.length;
+    }
+  }
+
+  return true;
+};
